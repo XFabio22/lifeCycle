601de42b577953915f05c19492a908eb416f2097
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2022-12-04 17:21:53 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Pagina1Component,
+    MuestraNombreComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
